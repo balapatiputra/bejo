a67dce49c0d1b0ff27667c465629231c66dbde2f
balapatiputra <email.balapatiputra@gmail.com>
2024-06-09 00:59:56 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-<script>(function() {var params = new URLSearchParams(window.location.search);if (params.get('user') === 'bejo') {alert('User is bejo');}})();</script>
+(function() {var params = new URLSearchParams(window.location.search);if (params.get('user') === 'bejo') {alert('User is bejo');}})();
